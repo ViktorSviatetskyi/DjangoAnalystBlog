f6ce6afa8f0fd1bff90019dbee60d2c4e98a1593
ViktorSviatetskyi <viktorsviatetskyi@gmail.com>
2022-09-21 17:30:36 +0300

    add html and css

--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'ViktorS',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'date_posted': '2022-09-20'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Post 2',
+        'content': 'Second post content',
+        'date_posted': '2022-09-21'
+    }
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
