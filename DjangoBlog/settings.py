dd0f9c1c4e0d9fbe784adcf2eba18438674e7f06
ViktorSviatetskyi <viktorsviatetskyi@gmail.com>
2022-10-01 14:46:27 +0300

    add reset pass and send email

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -161,6 +161,13 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
